19adda115dbd9446cc85e39dda6cf8a005297721
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-05-02 14:44:38 -0300

    :sparkles: Add: funcionalidades de edição mais toltips funcionais para editar o texto, extensões lowlight e codeblock do tiptap

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Editor from "./components/Editor";
+import {Editor} from "./components/Editor";
 
 export function App() {
   return (
     <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-indigo-300 to-purple-400">
-      <div className="bg-gray-200 sm:w-[1000px] mx-auto rounded-lg min-h-[800px] shadow-md border overflow-hidden border-black/20 grid grid-cols-[16rem_1fr]">
-        <aside className="bg-zinc-50 border-r border-r-zinc-100 p-4">
+      <div className="bg-zinc-800 sm:w-[1000px] mx-auto rounded-lg min-h-[800px] shadow-md border overflow-hidden border-black/20 grid grid-cols-[12rem_1fr]">
+        <aside className="bg-zinc-900 border-r border-r-zinc-100 p-4">
           <div className="flex gap-2 group">
-            <button className="w-3 h-3 rounded-full bg-zinc-300 group-hover:bg-red-400"></button>
-            <button className="w-3 h-3 rounded-full bg-zinc-300 group-hover:bg-yellow-400"></button>
-            <button className="w-3 h-3 rounded-full bg-zinc-300 group-hover:bg-green-400"></button>
+            <span className="w-3 h-3 rounded-full bg-red-400 hover:bg-red-500"></span>
+            <span className="w-3 h-3 rounded-full bg-yellow-400 hover:bg-yellow-500"></span>
+            <span className="w-3 h-3 rounded-full bg-green-400 hover:bg-green-500"></span>
           </div>
         </aside>
         <main className="p-4">
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,82 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
-export default function Editor() {
+import { CodeBlockLowlight } from "@tiptap/extension-code-block-lowlight";
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import { createLowlight } from "lowlight";
+import "highlight.js/styles/tokyo-night-dark.css";
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+  RxListBullet,
+} from "react-icons/rx";
+
+const lowlight = createLowlight();
+
+lowlight.register({ css });
+lowlight.register({ js });
+lowlight.register({ ts });
+lowlight.register({ html });
+
+export function Editor() {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, CodeBlockLowlight.configure({ lowlight })],
     content: initialContent,
+    editorProps: {
+      attributes: {
+        class: "outline-none p-2",
+      },
+    },
   });
   return (
-    <EditorContent
-      editor={editor}
-      className="max-w[700px] mx-auto pt-16 prose prose-violet"
-    />
+    <>
+      <EditorContent
+        editor={editor}
+        className="max-w[700px] mx-auto pt-16 prose prose-violet prose-invert"
+      />
+      {editor && (
+        <BubbleMenu
+          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg flex overflow-hidden divide-zinc-400 gap-2"
+          editor={editor}
+        >
+          <button
+            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleBold().run()}
+          >
+            <RxFontBold className="w-4 h-4" />
+          </button>
+          <button
+            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+          >
+            <RxFontItalic className="w-4 h-4" />
+          </button>
+          <button
+            className="p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+          >
+            <RxStrikethrough className="w-4 h-4" />
+          </button>
+          <button
+            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          >
+            <RxCode className="w-4 h-4" />
+          </button>
+
+          <button
+            className=" p-2 text-zinc-600 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+          >
+            <RxListBullet className="w-4 h-4" />
+          </button>
+        </BubbleMenu>
+      )}
+    </>
   );
 }
