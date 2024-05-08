40c11c1dd842b2d7e55e7a688c579b6b1d8c4d68
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-05-08 00:34:29 -0300

    :sparkles: feat: Adicionar extensão FloatingMenu ao tiptap

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Editor} from "./components/Editor";
 export function App() {
   return (
     <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-indigo-300 to-purple-400">
-      <div className="bg-zinc-800 sm:w-[1000px] mx-auto rounded-lg min-h-[800px] shadow-md border overflow-hidden border-black/20 grid grid-cols-[12rem_1fr]">
+      <div className="bg-zinc-800 sm:w-[1100px] mx-auto rounded-lg min-h-[720px] shadow-md border overflow-hidden border-black/20 grid grid-cols-[12rem_1fr]">
         <aside className="bg-zinc-900 border-r border-r-zinc-100 p-4">
           <div className="flex gap-2 group">
             <span className="w-3 h-3 rounded-full bg-red-400 hover:bg-red-500"></span>
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,9 @@
-import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
 import { CodeBlockLowlight } from "@tiptap/extension-code-block-lowlight";
@@ -14,7 +19,10 @@ import {
   RxStrikethrough,
   RxCode,
   RxListBullet,
+  RxChevronDown,
+  RxChatBubble,
 } from "react-icons/rx";
+import { BubbleButton } from "./BubbleButton";
 
 const lowlight = createLowlight();
 
@@ -37,44 +45,101 @@ export function Editor() {
     <>
       <EditorContent
         editor={editor}
-        className="max-w[700px] mx-auto pt-16 prose prose-violet prose-invert"
+        className="max-w-[700px] mx-auto pt-16 prose prose-violet prose-invert"
       />
       {editor && (
+        <FloatingMenu
+          className="bg-zinc-700 shadow-xl border text-white border-zinc-600 gap-1 shadow-black/20 rounded-lg flex flex-col py-2 px-1"
+          editor={editor}
+          shouldShow={({ state }) => {
+            const { $from } = state.selection;
+            const currentLineText = $from.nodeBefore?.textContent;
+            return currentLineText === ".";
+          }}
+        >
+          <button className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600">
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">
+                
+                Just start writting with plain text
+              </span>
+            </div>
+          </button>
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+
+          >
+            <img
+              src="http://www.notion.so/images/blocks/header.57a7576a.png"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">
+                Just start writting with plain text
+              </span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
+      {editor && (
         <BubbleMenu
-          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg flex overflow-hidden divide-zinc-400 gap-2"
+          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg flex divide-x divide-zinc-400"
           editor={editor}
         >
-          <button
-            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
-            onClick={() => editor.chain().focus().toggleBold().run()}
+          <BubbleButton
+            onClick={() => editor.chain().focus().createParagraphNear().run()}
           >
-            <RxFontBold className="w-4 h-4" />
-          </button>
-          <button
-            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
-            onClick={() => editor.chain().focus().toggleItalic().run()}
+            Text
+            <RxChevronDown className="w-4 h-4" />
+          </BubbleButton>
+          <BubbleButton
+            onClick={() => editor.chain().focus().createParagraphNear().run()}
           >
-            <RxFontItalic className="w-4 h-4" />
-          </button>
-          <button
-            className="p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-          >
-            <RxStrikethrough className="w-4 h-4" />
-          </button>
-          <button
-            className=" p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
-            onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          >
-            <RxCode className="w-4 h-4" />
-          </button>
+            <RxChatBubble className="w-4 h-4" />
+            Comment
+          </BubbleButton>
+          <div className="flex items-center">
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              data-active={editor.isActive("bold")}
+            >
+              <RxFontBold className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              data-active={editor.isActive("italic")}
+            >
+              <RxFontItalic className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              data-active={editor.isActive("strike")}
+            >
+              <RxStrikethrough className="w-4 h-4" />
+            </BubbleButton>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+              data-active={editor.isActive("code")}
+            >
+              <RxCode className="w-4 h-4" />
+            </BubbleButton>
 
-          <button
-            className=" p-2 text-zinc-600 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
-            onClick={() => editor.chain().focus().toggleBulletList().run()}
-          >
-            <RxListBullet className="w-4 h-4" />
-          </button>
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              data-active={editor.isActive("list")}
+            >
+              <RxListBullet className="w-4 h-4" />
+            </BubbleButton>
+          </div>
         </BubbleMenu>
       )}
     </>
