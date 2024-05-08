322d0157a79e9ea78774863e55c4a4be113e3f91
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-05-08 01:49:42 -0300

    :lipstick: feat: Melhorando visualização

--- src/components/BubbleButton.tsx
+++ src/components/BubbleButton.tsx
@@ -7,7 +7,7 @@ interface BubbleButtonProps extends ComponentProps<"button"> {
 export function BubbleButton(props: BubbleButtonProps) {
   return (
     <button
-      className="p-2 text-zinc-200 gap-1.5 text-sm flex items-center font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:bg-zinc-600"
+      className="p-2 text-zinc-200 gap-1.5 text-sm flex items-center font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-violet-400"
       {...props}
     />
   );
